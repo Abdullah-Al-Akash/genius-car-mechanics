081c3684e18769f060350c124dec92679938f895
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-10-14 23:22:02 +0600

    Added useAuth and new Context

--- src/App.js
+++ src/App.js
@@ -11,30 +11,33 @@ import NotFound from './Pages/NotFound/NotFound';
 import Booking from './Pages/Booking/Booking';
 import Login from './Pages/Login/Login/Login';
 import Header from './Pages/Shared/Header/Header';
+import AuthProvider from './contexts/AuthProvider';
 
 function App() {
   return (
     <div>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/booking/:serviceId">
-            <Booking />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/booking/:serviceId">
+              <Booking />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
